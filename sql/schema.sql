543b2938b323283295cf8097c068edbb34d1ece9
Elliot Whaley <ewhaley@bluegracegroup.com>
2026-07-09 08:49:46 -0400

    schema change

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,7 @@ create extension if not exists "pgcrypto";
 
 create table if not exists public.performances (
   id uuid primary key default gen_random_uuid(),
-  athlete text not null default 'Sean Williams',
+  athlete text not null default 'Elliot W',
   event_name text not null,
   mark text not null,
   mark_value numeric,
